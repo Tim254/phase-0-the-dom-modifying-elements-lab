9ada1081d7178dc14088eca1633acaa8c995f33c
Tim254 <tim.mailu@gmail.com>
2022-06-03 02:00:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "YOUR-NAME is the champion";
